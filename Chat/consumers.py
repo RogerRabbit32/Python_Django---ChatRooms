d7c3626f38525f1fab959efb50b344392092d695
Kirill Levin <levinkirill@yandex.ru>
2023-05-25 16:49:50 +0300

    Handled message creation within WS connection

--- Chat/consumers.py
+++ Chat/consumers.py
@@ -1,35 +1,92 @@
 import json
-
 from channels.generic.websocket import AsyncWebsocketConsumer
+from channels.db import database_sync_to_async
+from .models import PrivateChat, Message
+from .serializers import MessageSerializer
 
 
 class ChatConsumer(AsyncWebsocketConsumer):
     async def connect(self):
-        self.room_name = self.scope["url_route"]["kwargs"]["room_name"]
-        self.room_group_name = "chat_%s" % self.room_name
+        # Get the chat ID from the URL or request parameters
+        self.chat_id = self.scope['url_route']['kwargs']['chat_id']
+        self.room_group_name = "chat_%s" % self.chat_id
+        # Check if the user is authorized to access the chat
+        chat = await self.get_chat(self.chat_id)
+        if not chat:
+            await self.close()
+            return
 
-        # Join room group
-        await self.channel_layer.group_add(self.room_group_name, self.channel_name)
+        # Add the user to the chat group
+        await self.channel_layer.group_add(
+            self.room_group_name,
+            self.channel_name
+        )
 
+        # Accept the WebSocket connection
         await self.accept()
 
     async def disconnect(self, close_code):
-        # Leave room group
-        await self.channel_layer.group_discard(self.room_group_name, self.channel_name)
+        # Remove the user from the chat group
+        self.chat_id = self.scope['url_route']['kwargs']['chat_id']
+        chat = await self.get_chat(self.chat_id)
+        if chat:
+            await self.channel_layer.group_discard(
+                self.room_group_name,
+                self.channel_name
+            )
 
-    # Receive message from WebSocket
     async def receive(self, text_data):
-        text_data_json = json.loads(text_data)
-        message = text_data_json["message"]
+        # Parse the received message
+        message_data = self.parse_message(text_data)
 
-        # Send message to room group
+        # Get the chat ID from the URL or request parameters
+        chat_id = self.scope['url_route']['kwargs']['chat_id']
+
+        # Check if the user is authorized to access the chat
+        chat = await self.get_chat(chat_id)
+        if not chat:
+            await self.close()
+            return
+
+        # Create and save the message to the database
+        message = await self.create_message(chat, message_data)
+
+        # Serialize the message
+        serialized_message = MessageSerializer(message).data
+
+        # Broadcast the message to all connected clients
         await self.channel_layer.group_send(
-            self.room_group_name, {"type": "chat_message", "message": message}
+            self.room_group_name,
+            {
+                'type': 'chat_message',
+                'message': serialized_message
+            }
         )
 
-    # Receive message from room group
     async def chat_message(self, event):
-        message = event["message"]
+        # Send the received chat message to the client
+        message = event['message']
+        await self.send(text_data=message['text'])
 
-        # Send message to WebSocket
-        await self.send(text_data=json.dumps({"message": message}))
+    @database_sync_to_async
+    def get_chat(self, chat_id):
+        try:
+            chat = PrivateChat.objects.get(id=chat_id)
+            if self.scope["user"] == chat.user1 or self.scope["user"] == chat.user2:
+                return chat
+        except PrivateChat.DoesNotExist:
+            return None
+
+    @database_sync_to_async
+    def create_message(self, chat, message_data):
+        message = Message.objects.create(
+            content_object=chat,
+            sender=self.scope['user'],
+            text=message_data['text']
+        )
+        return message
+
+    def parse_message(self, text_data):
+        # Parse the received message data (adjust as per your message format)
+        message_data = json.loads(text_data)
+        return message_data
